6f94ad83bdae664e83c2f884e8e71e423843e64d
Tehjasonlam <129552222+Tehjasonlam@users.noreply.github.com>
2024-12-12 02:58:14 -0600

    Delete R directory

--- R/hw22.r
+++
@@ -1,8 +0,0 @@
-
-Codata = read.delim("https://www.math.uh.edu/~charles/data/Montana.txt")
-class(Codata)
-names(Codata)
-
-barplot(table(Codata$Region))
-barplot(table(Codata$State))
-barplot(table(Codata$Politics))
